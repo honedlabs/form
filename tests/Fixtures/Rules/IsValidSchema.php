d008211d8a54dd37b3156388d57079af185d8b80
jdw5 <joshuadwallace1@gmail.com>
2025-04-10 22:56:48 +0000

    feat(core): has any and has all methods

--- tests/Fixtures/Rules/IsValidSchema.php
+++ tests/Fixtures/Rules/IsValidSchema.php
@@ -30,7 +30,7 @@ class IsValidSchema extends ValidationRule implements HasTypescript
             'price' => [
                 'value' => 'number',
                 'currency' => 'string | null',
-            ],
-        ];
+            ]
+        ]; 
     }
 }
