2e0534dc587997295323b2c2b2e620c426f685c8
jdw5 <joshuadwallace1@gmail.com>
2025-04-13 03:22:43 +0000

    chore(action): bump core

--- src/Concerns/HasForm.php
+++ src/Concerns/HasForm.php
@@ -8,33 +8,33 @@ use Honed\Form\Form;
 
 /**
  * @template TModel of \Illuminate\Database\Eloquent\Model
- *
+ * 
  * @phpstan-require-extends \Illuminate\Foundation\Http\FormRequest
- */
+ */ 
 trait HasForm
 {
     /**
      * The underlying form object.
-     *
+     * 
      * @var \Honed\Form\Form|null
      */
     protected $form;
 
     /**
      * Convert the form request to a form object.
-     *
+     * 
      * @return \Honed\Form\Form
      */
     public static function asForm()
     {
-        $request = new self;
+        $request = new self();
 
         return $request->formInstance();
     }
 
     /**
      * Get an instance of the form object.
-     *
+     * 
      * @return \Honed\Form\Form
      */
     public function formInstance()
@@ -44,15 +44,18 @@ trait HasForm
 
     /**
      * Set the configuration to use for the form via a callback.
-     *
-     * @param  \Honed\Form\Form  $form
+     * 
+     * @param \Honed\Form\Form $form
      * @return void
      */
-    public function form($form) {}
+    public function form($form)
+    {
+        return;
+    }
 
     /**
      * Get the validated data after it has been processed by the form.
-     *
+     * 
      * @return array<string, mixed>
      */
     public function getFormData()
@@ -66,8 +69,8 @@ trait HasForm
 
     /**
      * Use the form configuration to store a new model record.
-     *
-     * @param  class-string<TModel>  $modelClass
+     * 
+     * @param class-string<TModel> $modelClass
      * @return TModel
      */
     public function store($modelClass)
@@ -79,8 +82,8 @@ trait HasForm
 
     /**
      * Use the form configuration to update an existing model record.
-     *
-     * @param  TModel  $model
+     * 
+     * @param TModel $model
      * @return int
      */
     public function update($model)
@@ -91,8 +94,8 @@ trait HasForm
 
     /**
      * Use the form configuration to delete a model record.
-     *
-     * @param  TModel  $model
+     * 
+     * @param TModel $model
      * @return mixed
      */
     public function destroy($model)
